a5ec3c57cd4ba9953c92f64b84b129331c26745c
codenot_04 <swarna.c.murmu04@gmail.com>
2026-06-23 21:35:30 +0530

    Add files via upload

---
+++ 947_Most_Stones_Removed/most_stones_removed.cpp
@@ -0,0 +1,87 @@
+class DisjointSet {
+public:
+    vector<int> parent, rank, size;
+
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int maxrow=0;
+        int maxcol=0;
+        int n=stones.size();
+        for(auto it:stones){
+            maxrow=max(maxrow,it[0]);
+            maxcol=max(maxcol,it[1]);
+        }
+        DisjointSet ds(maxrow+maxcol+1);
+        unordered_map<int,int>stonenodes;
+        for(auto it:stones){
+            int noderow=it[0];
+            int nodecol=it[1]+maxrow+1;
+            ds.unionBySize(noderow,nodecol);
+            stonenodes[noderow]=1;
+            stonenodes[nodecol]=1;
+
+        }
+        int count=0;
+        for(auto it:stonenodes){
+            if(ds.findUPar(it.first)==it.first){
+                count++;
+            }
+        }
+        return n-count;
+        
+    }
+};
